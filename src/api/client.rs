8c60ec387c7ce318cbce36e7bca8ccffc3326bc3
Amaan <amaanarmaanmuhammad@gmail.com>
2026-07-26 17:34:20 +0400

    added client

--- src/api/client.rs
+++ src/api/client.rs
@@ -1,5 +1,7 @@
-pub(crate) fn get() {
-    todo!(
-        "make a client"
-    )
+use reqwest::Client;
+
+pub(crate) fn get() -> reqwest::Result<Client> {
+    Client::builder()
+        .user_agent("track/0.1.0")
+        .build()
 }
