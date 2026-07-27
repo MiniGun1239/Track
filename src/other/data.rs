4e33a3716d2f9e9a707b61e06dbe1d5033587279
Amaan <amaanarmaanmuhammad@gmail.com>
2026-07-27 22:51:53 +0400

    added airport

--- src/other/data.rs
+++ src/other/data.rs
@@ -8,6 +8,7 @@ pub(crate) struct AdsbLOLResponse {
 #[derive(Deserialize, Debug, Clone)]
 pub(crate) struct AircraftData {
     pub(crate) flight: Option<String>,
+    pub(crate) t: Option<String>,
     pub(crate) r: Option<String>,
     pub(crate) alt_baro: Option<String>,
 
@@ -16,4 +17,14 @@ pub(crate) struct AircraftData {
 
     pub(crate) lat: Option<f64>,
     pub(crate) lon: Option<f64>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub(crate) struct AirportData {
+    pub(crate) countryiso2: Option<String>,
+    pub(crate) iata: Option<String>,
+    pub(crate) icao: Option<String>,
+
+    pub(crate) lat: Option<f32>,
+    pub(crate) lon: Option<f32>,
 }
