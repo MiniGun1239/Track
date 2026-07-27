af0d06148e68c983d688db3f3986c577a4f99103
Amaan <amaanarmaanmuhammad@gmail.com>
2026-07-27 17:10:46 +0400

    refactored

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,6 @@ use crate::cli::cli_args::CliArgs;
 use crate::cli::handle;
 use crate::cli::parse_flags::parse;
 
-pub mod cli;
-mod callsign;
-pub mod registration;
-pub mod api;
-
 pub fn main() {
     let args: CliArgs = parse();
 
