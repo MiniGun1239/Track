74e19a135dd984627cbbfebc0ddfc25ee2fd0256
Amaan <amaanarmaanmuhammad@gmail.com>
2026-07-28 23:36:50 +0400

    added refs

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,4 +1,6 @@
+use std::error::Error;
 use reqwest::Client;
+use crate::other::data::{AircraftData, AirportData, RouteResponse};
 
 pub mod client;
 pub mod plane;
@@ -6,4 +8,12 @@ pub mod route;
 
 pub fn client() -> reqwest::Result<Client> {
     client::get()
+}
+
+pub fn plane_data(client: Client, endpoint_url: &str) -> Result<Option<AircraftData>, Box<dyn Error>> {
+    plane::data(client, endpoint_url)
+}
+
+pub fn route_data(client: Client, endpoint_url: &str) -> Result<Option<RouteResponse>, Box<dyn Error>> {
+    route::data(client, endpoint_url)
 }
