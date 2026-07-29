013ad9b1beff99c0f5cbd5067368000a486b5154
Amaan <amaanarmaanmuhammad@gmail.com>
2026-07-29 14:15:42 +0400

    made async

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -10,8 +10,8 @@ pub fn client() -> reqwest::Result<Client> {
     client::get()
 }
 
-pub fn plane_data(client: Client, url_endpoint: &str) -> Result<Option<AircraftData>, Box<dyn Error>> {
-    plane::data(client, url_endpoint)
+pub async fn plane_data(client: Client, url_endpoint: &str) -> Result<Option<AircraftData>, Box<dyn Error>> {
+    plane::data(client, url_endpoint).await
 }
 
 pub fn route_data(client: Client, url_endpoint: &str) -> Result<Option<RouteResponse>, Box<dyn Error>> {
