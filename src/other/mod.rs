fdf7a98b19f61e00ef323291ff75078d3bffce4b
Amaan <amaanarmaanmuhammad@gmail.com>
2026-07-31 21:24:31 +0400

    added ts

--- src/other/mod.rs
+++ src/other/mod.rs
@@ -5,3 +5,25 @@ use crate::other::distance::{
 
 pub mod data;
 pub mod distance;
+
+pub fn distance(
+    (lat_dep, lon_dep): (f64, f64),
+    (lat_dest, lon_dest): (f64, f64)
+) -> f64 {
+    distance_between_airports(
+        (lat_dep, lon_dep),
+        (lat_dest, lon_dest)
+    )
+}
+
+pub fn ratio(
+    (lat_plane, lon_plane): (f64, f64),
+    (lat_dep, lon_dep): (f64, f64),
+    (lat_dest, lon_dest): (f64, f64)
+) -> f64 {
+    distance_ratio(
+        (lat_plane, lon_plane),
+        (lat_dep, lon_dep),
+        (lat_dest, lon_dest)
+    )
+}
