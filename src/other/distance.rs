ec43811a46e2f169f8284381694585b20d1ebfe5
Amaan <amaanarmaanmuhammad@gmail.com>
2026-07-31 20:02:46 +0400

    optimizations

--- src/other/distance.rs
+++ src/other/distance.rs
@@ -1,27 +1,39 @@
 static EARTH_RADIUS: f64 = 6371.0;
 
-pub(crate) fn distance(
+pub(crate) fn distance_between_airports(
     (lat1, lon1): (f64, f64),
     (lat2, lon2): (f64, f64)
 ) -> f64 {
 
-    haversine_formula(lat1, lon1, lat2, lon2)
+    haversine_formula(
+        (lat1, lon1),
+        (lat2, lon2)
+    )
 }
 
 
-pub(crate) fn percent_distance(
+pub(crate) fn distance_ratio(
     (lat_plane, lon_plane): (f64, f64),
     (lat_dep, lon_dep): (f64, f64),
     (lat_dest, lon_dest): (f64, f64)
 ) -> f64 {
-    let dep_dest_distance = haversine_formula(lat_dep, lon_dep, lat_dest, lon_dest);
-    let plane_airport_distance = haversine_formula(lat_plane, lon_plane, lat_dest, lon_dest);
+    let dep_dest_distance = haversine_formula(
+        (lat_dep, lon_dep),
+        (lat_dest, lon_dest)
+    );
+    let plane_airport_distance = haversine_formula(
+        (lat_plane, lon_plane),
+        (lat_dest, lon_dest)
+    );
 
     (dep_dest_distance / plane_airport_distance) * 100.0
 }
 
 
-fn haversine_formula(mut phi_1: f64, mut lambda_1: f64, mut phi_2: f64, mut lambda_2: f64) -> f64 {
+fn haversine_formula(
+    (lat_1, lon_1): (f64, f64), 
+    (lat_2, lon_2): (f64, f64)
+) -> f64 {
 // Formula:
     // Delta Sigma
     // = 2arcsin(
@@ -31,11 +43,11 @@ fn haversine_formula(mut phi_1: f64, mut lambda_1: f64, mut phi_2: f64, mut lamb
     //      )
     //  )
 
-    phi_1 = phi_1.to_radians();
-    lambda_1 = lambda_1.to_radians();
+    let phi_1 = lat_1.to_radians();
+    let lambda_1 = lon_1.to_radians();
 
-    phi_2 = phi_2.to_radians();
-    lambda_2 = lambda_2.to_radians();
+    let phi_2 = lat_2.to_radians();
+    let lambda_2 = lon_2.to_radians();
 
     let d_phi: f64 = phi_2 - phi_1;
     let d_lambda: f64 = lambda_1 - lambda_2;
