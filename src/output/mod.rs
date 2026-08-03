3cb7e0bcc8885bb0e147cb730a924645aa4e88dd
Amaan <amaanarmaanmuhammad@gmail.com>
2026-08-03 17:02:59 +0400

    refactor

--- src/output/mod.rs
+++ src/output/mod.rs
@@ -5,29 +5,9 @@ pub(crate) mod altitude;
 pub(crate) mod full;
 
 pub(crate) fn progress(
-    callsign: String,
-    reg: String,
-    plane_type: String,
-    alt_baro: String,
-    gs: f32,
-    plane_lat: f64,
-    plane_lon: f64,
-
-    dep_name: &str,
-    dep_lat: f64,
-    dep_lon: f64,
-    dep_country: &str,
-    dep_icao: &str,
-    dep_iata: &str,
-    dep_airport: &str,
-
-    dest_name: &str,
-    dest_lat: f64,
-    dest_lon: f64,
-    dest_country: &str,
-    dest_icao: &str,
-    dest_iata: &str,
-    dest_airport: &str,
+    callsign: String, reg: String, plane_type: String, alt_baro: String, gs: f32, plane_lat: f64, plane_lon: f64,
+    dep_name: &str, dep_lat: f64, dep_lon: f64, dep_country: &str, dep_icao: &str, dep_iata: &str, dep_airport: &str,
+    dest_name: &str, dest_lat: f64, dest_lon: f64, dest_country: &str, dest_icao: &str, dest_iata: &str, dest_airport: &str,
 ) {
     todo!(
         "call progress::output"
