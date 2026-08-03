156e0bc536aa42ea62a1d80a561c999f2cf5a34d
Amaan <amaanarmaanmuhammad@gmail.com>
2026-08-03 17:03:04 +0400

    refactor

--- src/output/mod.rs
+++ src/output/mod.rs
@@ -9,8 +9,10 @@ pub(crate) fn progress(
     dep_name: &str, dep_lat: f64, dep_lon: f64, dep_country: &str, dep_icao: &str, dep_iata: &str, dep_airport: &str,
     dest_name: &str, dest_lat: f64, dest_lon: f64, dest_country: &str, dest_icao: &str, dest_iata: &str, dest_airport: &str,
 ) {
-    todo!(
-        "call progress::output"
+    progress::output(
+        callsign, reg, plane_type, alt_baro, gs, plane_lat, plane_lon,
+        dep_name, dep_lat, dep_lon, dep_country, dep_icao, dep_iata, dep_airport,
+        dest_name, dest_lat, dest_lon, dest_country, dest_icao, dest_iata, dest_airport,
     )
 }
 
